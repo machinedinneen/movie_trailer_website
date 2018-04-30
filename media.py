657b55f887587342bcc5be043de6ee32aa143536
machinedinneen <38298694+machinedinneen@users.noreply.github.com>
2018-04-30 13:56:19 -1000

    Comment changes

--- media.py
+++ media.py
@@ -1,6 +1,11 @@
 import webbrowser
 
 class Movie():
+    
+    # static variable for available rating
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    
+    # setting variables for movie definitions
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
@@ -8,5 +13,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # opening webbrowser for specified movie
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
